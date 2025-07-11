7c5696675150bfd11a6a7ef4d68ae71e70352ae8
taniguchi.tatsuya <tatsuya19951204@gmail.com>
2025-07-11 18:38:27 +0900

    サンプルアプリ① 課題　問題３

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if not "initialized" in st.session_state:
 cn.display_app_title()
 
 # モード表示
-cn.display_select_mode()
+cn.sidebar_mode_selection()
 
 # AIメッセージの初期表示
 cn.display_initial_ai_message()
